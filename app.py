4d3540c9e63efa87757f7e129407864f23094220
lulamae12 <tls.tommy11@gmail.com>
2024-01-29 12:30:43 -0500

    COMMIT FOR NOW

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify,request
 import os,random,time,signal
+from hurry.filesize import size
+import datetime
+
+from werkzeug.utils import secure_filename
+
+import requests
 
 
 PATH = os.path.dirname(os.path.dirname(__file__))
@@ -22,12 +28,32 @@ file_list = []
 
 for file in os.listdir(app.config['DEVICE_FILES_FOLDER']):
     stat = os.stat(os.path.join(DEVICE_FILES_FOLDER,file))
-    dataDict = {'name':file,'size':stat.st_size,'last_modified':stat.st_mtime}
+
+    #convert sizes to appropriate units and times from unix
+    dataDict = {'name':file,'size':size(stat.st_size),'last_modified':datetime.datetime.fromtimestamp(stat.st_mtime).strftime("%d/%m/%Y, %H:%M:%S")}
     file_list.append(dataDict)
 
+#upload files to deviceS
+@app.route('/upload', methods=['POST'])
+def uploadToDevice():
+    print("upload ran!")
+    print(request)
+    print("req ",request.files)    
+    files = request.files.getlist('file')
+    for file in files:
+        print(file)
+        if file:
+            print("valid file")
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['DEVICE-FILES-FOLDER'], filename))
+            filename = os.path.join(app.config['DEVICE-FILES-FOLDER'],filename)
+        else:
+            print("invalid file")
+           
+    return jsonify({"success": True})
+    
 
-
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 @app.route("/index")
 def index():
     return render_template("index.html", file_list=file_list)
