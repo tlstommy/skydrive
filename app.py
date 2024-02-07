5fbbd3e48f0a8ad27fea7d3297eef13817eba3bc
lulamae12 <tls.tommy11@gmail.com>
2024-02-07 16:33:17 -0500

    basic details setup

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from werkzeug.utils import secure_filename
 
 import requests
 
+# npx tailwindcss -i ./static/src/input.css -o ./static/dist/css/output.css --watch
 
 PATH = os.path.dirname(os.path.dirname(__file__))
 DEVICE_FILES_FOLDER = os.path.join(os.getcwd(),"device-files")
@@ -23,7 +24,6 @@ app = Flask(__name__)
 app.config['DEVICE_FILES_FOLDER'] = DEVICE_FILES_FOLDER
 
 
-
 def getFileList():
     #files
     file_list_names = os.listdir(app.config['DEVICE_FILES_FOLDER'])
@@ -31,19 +31,49 @@ def getFileList():
 
     for file in os.listdir(app.config['DEVICE_FILES_FOLDER']):
         stat = os.stat(os.path.join(DEVICE_FILES_FOLDER,file))
-
+        print(stat)
         #convert sizes to appropriate units and times from unix
-        dataDict = {'name':file,'size':size(stat.st_size),'last_modified':datetime.datetime.fromtimestamp(stat.st_mtime).strftime("%d/%m/%Y, %H:%M:%S")}
+        dataDict = {'inode':stat.st_ino, 'name':file,'size':size(stat.st_size),'last_modified':datetime.datetime.fromtimestamp(stat.st_mtime).strftime("%d/%m/%Y, %H:%M:%S")}
         file_list.append(dataDict)
     return file_list
 
-#file list
+# file list
 @app.route('/files', methods=['GET'])
 def listFiles():
     return jsonify(getFileList())
 
+# get details of singular file via inode
+@app.route('/get-file-details', methods=['GET'])
+def getFileInfo():
 
-#upload files to deviceS
+    fileDetailsDict = {}
+    print(request)
+    inode = int(request.args['inode'])
+    filename = request.args['name']
+    print(inode,filename)
+    stat = os.stat(os.path.join(DEVICE_FILES_FOLDER,filename))
+
+    # check if the files match
+    if(stat.st_ino != inode):
+        print("ERROR! Das a big problem.")
+
+    print(f"Filename: {filename}")
+    print(f"Inode: {stat.st_ino}")
+    print(f"Size: {stat.st_size} bytes")
+    print(f"Permissions: {oct(stat.st_mode)}")
+    print(f"Last modified: {stat.st_mtime}")
+
+    fileDetailsDict = {
+        "inode": stat.st_ino,
+        "filename": filename,
+        "size": size(stat.st_size),
+        "last_modified": datetime.datetime.fromtimestamp(stat.st_mtime).strftime("%d/%m/%Y, %H:%M:%S"),
+    }
+
+    return jsonify(fileDetailsDict)
+
+
+# upload files to deviceS
 @app.route('/upload', methods=['POST'])
 def uploadToDevice():
     print("upload ran!")
@@ -61,7 +91,7 @@ def uploadToDevice():
             print("invalid file")
            
     return jsonify({"success": True})
-    
+
 
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/index")
