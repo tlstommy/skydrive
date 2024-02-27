58cf0ad520b418d29b475efc0556ab951166f0c7
lulamae12 <tls.tommy11@gmail.com>
2024-02-27 13:39:25 -0500

    responsive to mobile now

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     "./static/src/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '640px', // Small screens
+        'md': '768px', // Medium screens
+        'lg': '1024px', // Large screens
+        'xl': '1280px', // Extra-large screens
+      },
+    },
   },
   plugins: [],
 }
