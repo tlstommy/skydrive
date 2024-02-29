7971017366afc63dba0c8c5ba06fc2ed8083bade
lulamae12 <tls.tommy11@gmail.com>
2024-02-28 19:15:54 -0500

    commit updated run.py

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from app import app
+import random
 
+
+#run.py
 if __name__ == '__main__':
-    app.run(debug=True,use_reloader=False)
+    app.secret_key = str(random.randint(100000,999999))
+    app.run(host="0.0.0.0",port=5000)
+    #app.run(debug=True,use_reloader=False)
